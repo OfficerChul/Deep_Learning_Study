66713f397fd0aa8f08be96887e8ee26158f835d7
Kyochul-Jang <jang128@purdue.edu>
2022-07-04 15:34:32 +0900

    Create assignment python file.

---
+++ Softmax_Regression/sofrtmax_MNIST.py
@@ -0,0 +1,10 @@
+"""
+* FILENAME : softmax_MNIST.py
+*
+* DESCRIPTION : softmax regressing using MNIST dataset and tensorflow v2.9.1
+*
+* AUTHOR : Kyochul Jang
+* EMAIL: jang128@purdue.edu
+* START DATE : 4 July 2022
+"""
+#%%
