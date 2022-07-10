5ff2d0beaae9ee5c6408c23f4023fcd23f5b76fd
Kyochul-Jang <jang128@purdue.edu>
2022-07-11 00:52:46 +0900

    Add edit

--- Softmax_Regression/sofrtmax_MNIST.py
+++ Softmax_Regression/sofrtmax_MNIST.py
@@ -7,4 +7,17 @@
 * EMAIL: jang128@purdue.edu
 * START DATE : 4 July 2022
 """
-#%%
+#%% Import Libraries
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% Version check tensorflow-cpu
+print('$ tensorflow version:', tf.__version__)
+print('$ Eagerly execution: ', tf.executing_eagerly())
+
+#%% Load MNIST dataset from implementing Keras API
+mnist = tf.keras.datasets.mnist
+a = 6
+mnist
+
